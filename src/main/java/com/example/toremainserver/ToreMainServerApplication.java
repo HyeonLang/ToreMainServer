18e73f87575e53857491e1a89d7ad9a14d41178c
HyeonLang <yoitsu0117@gmail.com>
2025-07-23 10:52:17 +0900

    Init ToreMainServer

---
+++ src/main/java/com/example/toremainserver/ToreMainServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.toremainserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToreMainServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToreMainServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/toremainserver/ToreMainServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toremainserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToreMainServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
